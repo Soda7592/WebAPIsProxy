91b0877b9656cdcfd752802ef7ed0250b4bea1a5
mata <mata7592@gapp.nthu.edu.tw>
2024-11-20 18:03:04 +0800

    feat: convert to json, Not successfully now.

--- httproxy.py
+++ httproxy.py
@@ -1,5 +1,6 @@
 from mitmproxy import http
 from mitmproxy import ctx
+import os
 import json
 
 class main:
@@ -7,18 +8,19 @@ class main:
         self.num = 0
         self.httpRequestlist = []
         self.domainName = None
-        self.load_http_requests()
-        self.writeIntoTxt = {
-            "ApiEndpointName":"", 
-            "Arguments":{
+        self.jsonDict = ""
+        self.loadFiles()
+        
 
-            }
-        }
-
-    def load_http_requests(self) :
+    def loadFiles(self) :
         try: 
             with open('httpRequest.txt', 'r') as f:
                 self.httpRequestlist = f.readlines()
+            if os.path.exists("ApiInfo.json") :
+                self.jsonDict = json.load("ApiInfo.json")
+            else :
+                with open('ApiInfo.json', 'w') as api :
+                    pass
         except FileExistsError as err:
             ctx.log.info(FileExistsError)
 
@@ -31,7 +33,7 @@ class main:
         if self.domainName in request.url :
             url = str(request.url)
             httpPostString =  url[url.find('.tw')+3:]
-            apisData = self.writeIntoTxt
+            # apisData = self.writeIntoTxt
             if httpPostString.find('?') != -1 :
                 httpPostString = httpPostString[:httpPostString.find('?')]
             try :
@@ -40,19 +42,21 @@ class main:
                         content = request.content.decode('utf-8')
                         postARG = request.get_text().split('&')
                         if len(postARG) and (httpPostString not in self.httpRequestlist) :
-                            self.httpRequestlist.append(httpPostString)       
+                            self.httpRequestlist.append(httpPostString)
                             if len(postARG[0]) != 0 :    
                                 f.write(httpPostString + '(' + request.method + ')' +   '\n')
-                                apisData['ApiEndpointName'] = httpPostString
+                                # apisData['ApiEndpointName'] = httpPostString
                                 argDict = {}
                                 for i in postARG :
                                         arg = i[:i.find('=')]
                                         val = i[i.find('=')+1:]
                                         argDict[arg] = val
                                         f.write(arg + ':' + val + '\n')
-                                        apisData['Arguments'] = argDict
-                                        with open('ApiInfo.json', 'a') as js :
-                                            js.write(apisData)
+                                        # apisData['Arguments'] = argDict
+                                        self.jsonDict[httpPostString] = argDict
+                                        json.dump(json.loads(self.jsonDict), 'ApiInfo.json')
+                                        # with open('ApiInfo.json', 'a') as js :
+                                        #     js.write(json.dumps(apisData) + '\n')
                         if len(content) != 0 and (httpPostString not in self.httpRequestlist) :
                             f.write(httpPostString + '(' + request.method + ')' +   '\n')
                             self.httpRequestlist.append(httpPostString)
