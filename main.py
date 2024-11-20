f32c31dcee83d990a02f989439fea7af90ab77e9
mata <mata7592@gapp.nthu.edu.tw>
2024-11-20 16:42:23 +0800

    Delete some useful comment in main.py

--- main.py
+++ main.py
@@ -57,29 +57,7 @@ class Joker:
                                 f.write(arg + ":" + val + '\n')
             except FileExistsError as err:
                 ctx.log.info(FileExistsError) 
-            # if httpPostString.find('?') == -1 :
-            #     pass
-            
-            # elif httpPostString[:httpPostString.find('?')] not in self.httpRequestlist :
-            #     self.httpRequestlist.append(httpPostString[:httpPostString.find('?')])
-            #     with open('httpRequest.txt', 'a') as f:
-            #         f.write("@@")
-            #         if httpPostString[httpPostString.find('?')] not in self.httpRequestlist :
-            #             f.write(httpPostString + '\n')
-            #             f.write("\n[+] "+ httpPostString[httpPostString.find('?'):]+ '\n\n')
-                        # f.write("\n" + httpPostString[httpPostString.find("?")+1:httpPostString.find('=')])
-
-        # info(request.url)   #(V) just URL and args
-        # info(request.method)  #(V) just POST, GET and so on...
-        # info(request.host)      #(V) just host, ex: www.dell.com
-        # info(str(request.port))   #(V) just website port ex: 443
-        # info(str(request.headers))  #(V) HTTP headers (include cookies) (類似攔截封包的時候會看到的下面的東西)
-        # info(str(request.cookies))    #(V) A Stored a lots cookies dict strcture
 
     def response(self, flow):
         response = flow.response
         info = ctx.log.info
-        # info(str(response.status_code))
-        # info(str(response.headers))
-        # info(str(response.cookies)) 
-        # info(str(response.text)) #(V) response just like curl http://<domain name>
